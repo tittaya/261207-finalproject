275ed649e15f7818a626d225dc3849d5dc68b610
nickkle891 <46592687+nickkle891@users.noreply.github.com>
2020-04-10 21:37:54 +0700

    about ready

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import '../App.css';
+import Logo from '../img/donutlogo.png';
+import { Link } from 'react-router-dom';
 
 function About(){
     return(
-        <div>
-            <h1>About Page</h1>
+        
+        <div className="container-about">
+            <img src={Logo} className="App-logo-small" />
+            
+            <h2>Developer</h2>
+           
+            <h5>Chinnakrit Ngeonsom 610610577</h5>
+        
+            <h5>Titaya Jitpang 610610579</h5>
+            
+            <h5>Samita Kedkaew 6106106620</h5>
+
+            <p className="forgot-password text-right">
+            Ready to order? <li><Link to="/home">ORDER NOW</Link></li>
+        </p>
+            
         </div>
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../App.css';
+import Logo from '../img/donutlogo.png';
+import { Link } from 'react-router-dom';
 
 function Home(){
     return(
-        <div>
-            <h1>Home Page</h1>
+        
+        <div className="container">
+            <img src={Logo} className="App-logo" />
+            
+            <ul className="App-link">
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/about">About</Link></li>
+            </ul>
         </div>
     );
 }
-
 export default Home;
