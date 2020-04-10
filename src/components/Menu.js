288de3cbe7e0712efdfe939cf4c582a94590bf0d
nickkle891 <46592687+nickkle891@users.noreply.github.com>
2020-04-10 20:21:04 +0700

    starter

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import '../App.css';
+import Logo from '../img/donutlogo.png';
+import { Link } from 'react-router-dom';
 
 function Menu(){
     return(
-        <div>
-            <p>Menu1</p>
-            <p>Menu2</p>
-            <p>Menu3</p>
+        <form className="menu">
+             <img src={Logo} className="App-logo-small" />
+        
+
+        <div className="container-menu">
+        <h3>Menu</h3>
+            <input type="username" className="form-control" placeholder="Enter username" />
         </div>
+
+
+        <div className="container-order">
+        <h3>Menu</h3>
+            <input type="username" className="form-control" placeholder="Enter username" />
+        </div>
+
+
+
+        
+    </form>
     );
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../App.css';
+import Logo from '../img/donutlogo.png';
+import { Link } from 'react-router-dom';
+
+
+
 
 function Welcome(){
     return(
-        <div>
-            <h1>Welcome</h1>
+        
+        <div className="container">
+            <img src={Logo} className="App-logo" />
+            
+            <ul className="App-link">
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+            </ul>
         </div>
     );
 }
